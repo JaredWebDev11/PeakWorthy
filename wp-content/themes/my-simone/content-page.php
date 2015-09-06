d3d47dfdc1ba31672ce5e4707b16e6e5aa3200c9
JaredWebDev11 <jarson925@gmail.com>
2015-09-06 14:35:02 -0700

    widget add and style
    added top and bottom widget to single pages for policy and dmca.
    
    deleted an un-needed style affecting new dmca page

--- wp-content/themes/my-simone/content-page.php
+++ wp-content/themes/my-simone/content-page.php
@@ -17,6 +17,7 @@
 	}
 	?>
 	<header class="entry-header">
+		<?php get_sidebar('top'); ?>  <!-- top ad widget here -->
 		<h1 class="entry-title"><?php the_title(); ?></h1>
 	</header><!-- .entry-header -->
 
@@ -29,5 +30,8 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
+	<div class="entry-header2">
+		<?php get_sidebar('bottom'); ?>  <!-- bottom ad widget here -->
+	</div>
 	<?php edit_post_link( __( 'Edit', 'my-simone' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
