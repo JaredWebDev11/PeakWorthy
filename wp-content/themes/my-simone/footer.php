0b88c4211b1f3e7dca001ca2142fbbe1425d1d1d
JaredWebDev11 <jarson925@gmail.com>
2015-09-08 13:27:28 -0700

    changed footer links for live site
    changed footer links from localhost to peakworthy.com links for working
    site to link correct pages

--- wp-content/themes/my-simone/footer.php
+++ wp-content/themes/my-simone/footer.php
@@ -13,16 +13,16 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
             <?php get_sidebar('footer'); ?>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://localhost', 'my-simone' ) ); ?>"><?php print'<div class="copyWrite">Peak Worthy Copywrite &copy 2015</div>'; ?></a>
-			<a href="<?php echo esc_url( __( 'http://localhost/dmca/', 'my-simone' ) ); ?>"><?php printf( __( 'DMCA', 'my-simone' ) ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com', 'my-simone' ) ); ?>"><?php print'<div class="copyWrite">Peak Worthy Copywrite &copy 2015</div>'; ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com/dmca/', 'my-simone' ) ); ?>"><?php printf( __( 'DMCA', 'my-simone' ) ); ?></a>
 			<span class="sep"> | </span>
-			<a href="<?php echo esc_url( __( 'http://localhost/privacy-policy/' ) ); ?>"><?php printf( __( 'Privacy Policy', 'my-simone') ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com/privacy-policy/' ) ); ?>"><?php printf( __( 'Privacy Policy', 'my-simone') ); ?></a>
 			<span class="sep"> | </span>
-			<a href="<?php echo esc_url( __( 'http://localhost/terms-of-use/', 'my-simone' ) ); ?>"><?php printf( __( 'Terms Of Use', 'my-simone') ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com/terms-of-use/', 'my-simone' ) ); ?>"><?php printf( __( 'Terms Of Use', 'my-simone') ); ?></a>
 			<span class="sep"> | </span>
-			<a href="<?php echo esc_url( __( 'http://localhost/fair-use-disclaimer/', 'my-simone' ) ); ?>"><?php printf( __( 'Fair Use Policy', 'my-simone' ) ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com/fair-use-disclaimer/', 'my-simone' ) ); ?>"><?php printf( __( 'Fair Use Policy', 'my-simone' ) ); ?></a>
 			<span class="sep"> | </span>
-			<a href="<?php echo esc_url( __( 'http://localhost/contact-us/', 'my-simone' ) ); ?>"><?php printf( __( 'Contact Us', 'my-simone' ) ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://peakworthy.com/contact-us/', 'my-simone' ) ); ?>"><?php printf( __( 'Contact Us', 'my-simone' ) ); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
