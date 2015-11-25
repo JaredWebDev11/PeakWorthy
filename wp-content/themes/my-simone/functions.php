7ac0ce028fa6b538de9af7a0d43d72912c165156
JaredWebDev11 <jarson925@gmail.com>
2015-11-24 16:25:19 -0800

    activated header widget ONLY no css
    added header widget to functions and added template php file. have not
    figured out positioning yet

--- wp-content/themes/my-simone/functions.php
+++ wp-content/themes/my-simone/functions.php
@@ -166,6 +166,15 @@ function my_simone_widgets_init() {
 		'before_title'  => '<h1 class="widget-title">',
 		'after_title'   => '</h1>',
 	) );
+	register_sidebar( array(
+		'name'          => __( 'Header Widget', 'my-simone' ),
+		'description'   => __( 'Widget that sits on top of header image.', 'my-simone'),
+		'id'            => 'sidebar-10',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 }
 add_action( 'widgets_init', 'my_simone_widgets_init' );
 
